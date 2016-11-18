1d4da59f57b53305990a2440bca7720718b71e85
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-11-17 20:55:54 -0700

    fixed template loading issue

--- reviews/views.py
+++ reviews/views.py
@@ -12,21 +12,21 @@ import datetime
 def review_list(request):
     latest_review_list = Review.objects.order_by('-pub_date')[:9]
     context = {'latest_review_list': latest_review_list}
-    return render(request, 'reviews/review_list.html', context)
+    return render(request, 'review_list.html', context)
 
 def review_detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    return render(request, 'reviews/review_detail.html', {'review': review})
+    return render(request, 'review_detail.html', {'review': review})
 
 def wine_list(request):
     wine_list = Wine.objects.order_by('-name')
     context = {'wine_list': wine_list}
-    return render(request, 'reviews/wine_list.html', context)
+    return render(request, 'wine_list.html', context)
 
 def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     form = ReviewForm()
-    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
+    return render(request, 'wine_detail.html', {'wine': wine, 'form': form})
 
 def add_review(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
@@ -47,5 +47,4 @@ def add_review(request, wine_id):
         # user hits the back button.
         return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
 
-    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
-    
+    return render(request, 'wine_detail.html', {'wine': wine, 'form': form})
